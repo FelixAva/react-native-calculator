029c2daabbf6d998242249797fa347f4c02df4d7
Felix Avalos <felavalosg@gmail.com>
2024-04-04 11:32:36 -0600

    Create app structure

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {StatusBar, StyleSheet, View} from 'react-native';
+import CalculatorScreen from './presentation/screens/CalculatorScreen';
+
+function App() {
+  return (
+    <View>
+      <StatusBar barStyle={'light-content'} backgroundColor="black" />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
